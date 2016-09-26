899d3a245a1f2d187d9d47a8f6b9e74c519813fd
Lore <lors2404@gmail.com>
2016-09-26 16:08:23 -0400

    ml_weight and SSE for reg

--- functions.py
+++ functions.py
@@ -9,29 +9,28 @@ import numpy.linalg as npl
 import numpy as np
 import math
 
- 
-def phi(X,M,f):
+def poli(x,n):
+    return x**n
+
+def phi(X,M,f=poli):
     n = len(X)
     phi_m = np.zeros((n,M+1))
     for i in range(n):
         for j in range(M+1):
             phi_m[i,j] = f(X[i],j)
     return phi_m
-    
-def poli(x,n):
-    return x**n
-    
+     
 def cos(x,n):
     return np.cos(math.pi*x*n)
     
-def ml_weight(X,Y,M,basis,l=0):
+def ml_weight(X,Y,M = 1,basis = poli ,l=0):
     '''
     Calculates the weights for linear regression
     Parameters:
-    X
-    Y
-    M
-    basis
+    X: vecotr of Nx1
+    Y: target vector of Nx1
+    M: dimensions of the function basis
+    basis: class of the basis
     l: lambda for regularization. If omitted is assumed to be 0
     '''
     m = phi(X,M,basis)  
@@ -39,3 +38,26 @@ def ml_weight(X,Y,M,basis,l=0):
     p1 = npl.inv(l*np.eye(M+1) + np.dot(m_t,m))
     p2 = np.dot(m_t,Y)
     return np.dot(p1,p2)
+    
+def SSE(w,L):
+    '''
+    Calculate sum of square residuals.
+    w: column vector resulting that represents the weights 
+    L is a dictinoary with the following values:
+    'X': X vector of Nx1
+    'Y' = target vector of Nx1    
+    OPTIONAL:
+    'M': dimension of the basis, otherwsie it assumes 1
+    'basis': basis por the transformation, otherwise it assumes poli
+    'l': lambda for regularization, otherwise it assumes 0
+    '''
+    #Extract parameters
+    X = L.get('X')
+    Y = L.get('Y')
+    M = L.get('M',1)
+    l = L.get('l',0) 
+    basis = L.get('basis',poli)
+    y_pred = np.dot(phi(X,M,basis),w)  
+    w_t = np.matrix.transpose(w)
+    return np.dot(np.matrix.transpose(Y-y_pred),Y-y_pred) + l*np.dot(w_t,w)   
+    
